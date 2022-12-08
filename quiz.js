eb1f41497b529f66c69a98efc84318823f8e52ab
Ayaan123456789 <110049364+Ayaan123456789@users.noreply.github.com>
2022-12-08 20:53:06 +0530

    Delete quiz.js

--- quiz.js
+++
@@ -1,9 +0,0 @@
-player1name = document.getElementById("player1_name").value;
-player2name = document.getElementById("player2_name").value;
-
-function login() {
-    localStorage.setItem("player1", player1name);
-    localStorage.setItem("player2", player2name);
-
-    window.location = "page2.html";
-}
