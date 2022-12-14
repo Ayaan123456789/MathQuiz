f04b63eccd159175be03a3083f363e2c581b1ebb
Ayaan123456789 <110049364+Ayaan123456789@users.noreply.github.com>
2022-12-14 12:04:06 +0530

    Update quiz-page2.js

--- quiz-page2.js
+++ quiz-page2.js
@@ -1,3 +1,10 @@
+queastion_turn = "player1";
+answer_turn = "player2";
+
+player1Score = 0;
+player2Score = 0;
+
+
 var player1Name= localStorage.getItem("player1");
 var player2Name= localStorage.getItem("player2");
 document.getElementById("player1_name").innerHTML = player1Name;
@@ -10,7 +17,57 @@ function Send(){
     console.log(actual_answer);
 
     document.getElementById("Question").innerHTML = number1 + "X" + number2;
-    
 
 }
+
+  
+
+function Check() {
+typedAnswer = document.getElementById("answer").value;
+
+
+
+if (actual_answer == typedAnswer) {
+
+    if (answer_turn == "player2"){
+      
+     answer_turn = "player1"   
+     document.getElementById("question_turn").innerHTML = "Player 2";
+     document.getElementById("answer_turn").innerHTML = "Player 1";
+   
+    }
+    else{
+        answer_turn = "player2"   
+        document.getElementById("question_turn").innerHTML = "Player 1";
+        document.getElementById("answer_turn").innerHTML = "Player 2";
+      
+
+    }
+   
+   
+    if (answer_turn == "player2"){
+      player2Score = player2Score + 1;
+      document.getElementById("player2_score").innerHTML = player2Score ;
+
+    }
+
+    else {
+        player1Score = player1Score + 1;
+        document.getElementById("player1_score").innerHTML = player1Score ;
+
+    }
+
+
+
+
+
+}
+
+document.getElementById("Question").innerHTML = "";
+document.getElementById("answer").value = "";
+document.getElementById("number_1").value = "";
+document.getElementById("number_2").value = "";
+
+}
+
 
